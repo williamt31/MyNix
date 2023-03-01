b42315d58cd402f9b325ae6417fcca38ec956713
William <42551399+williamt31@users.noreply.github.com>
2023-02-28 18:05:53 -0600

    Update buildiso.sh

--- scripts/buildiso.sh
+++ scripts/buildiso.sh
@@ -10,3 +10,35 @@ ISO_FILE="$VOLUME_PATH.iso"
 STORE="$BASE_PATH/built_isos"
 NAS="/mnt/share"
 
+# Delete $ISO_FILE If it exists
+if [ -f $STORE/$ISO_FILE ]
+then
+    echo -e "\nRemoving $ISO_FILE\n"
+    rm -f $STORE/$ISO_FILE
+fi
+
+# Copy updated config files into working files
+\cp CONFIG_FILES/ks*.cfg  $SOURCE_FILES/
+\cp CONFIG_FILES/grub.cfg $SOURCE_FILES/EFI/BOOT/
+
+# UEFI ISO Build
+xorriso -as mkisofs \
+-c isolinux/boot.cat \
+-b isolinux/isolinux.bin \
+-no-emul-boot \
+-boot-load-size 4 \
+-boot-info-table \
+-eltorito-alt-boot \
+-e images/efiboot.img \
+-no-emul-boot \
+-isohybrid-gpt-basdat \
+-o $STORE/$ISO_FILE \
+-V "$VOLUME_NAME" $SOURCE_FILES/ ### Quotes must stay here ###
+
+if [ -f $STORE/$ISO_FILE ]
+then
+    echo -e "\nMoving $ISO_FILE to the $NAS\n"
+    \mv $STORE/$ISO_FILE $NAS/
+else
+    echo -e "\nERROR, Unable to move: $ISO_FILE to $NAS\n"
+fi
